81bdd2ec7eaff01d6390a1a6adaf94564dbfbdd3
Mahdi Hasan Tazelly <mahdihasantazelly@gmail.com>
2025-01-21 17:11:24 +0600

    Looked on Multithreading

---
+++ MultiThreading.java
@@ -0,0 +1,38 @@
+package Notes;
+//Md. Mahdi Hasan Tazelly, SID: 2212193042
+public class MultiThreading {
+    public static void main(String[] args){
+        for(int i=0; i<5; i++){
+            MultiThread thread1 = new MultiThread("----Multithreading");
+            thread1.start();
+            MultiThread thread2 = new MultiThread("----Multithreading");
+            thread2.start();
+        }
+
+
+        MultiThread threadA = new MultiThread("normal method invoking");
+        MultiThread threadB = new MultiThread("normal method invoking");
+        for(int i=0; i<5; i++){
+            threadA.run();
+            threadB.run();
+        }
+    }
+}
+
+
+class MultiThread extends Thread{
+
+    String message;
+
+    MultiThread(String message){
+        this.message = message;
+    }
+    public void run(){
+        try{
+            System.out.println(Thread.currentThread().getName()+"----"+message);
+            Thread.sleep(500);
+        }catch(Exception e){
+            System.out.println("Error Occurred");
+        }
+    }
+}
